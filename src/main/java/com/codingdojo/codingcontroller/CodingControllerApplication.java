3a8c315c3755e109f25629540fd00fc1f31de95b
MyMac <mymac@Joels-MacBook-Pro.local>
2021-04-14 12:35:25 -0500

    first commit

---
+++ src/main/java/com/codingdojo/codingcontroller/CodingControllerApplication.java
@@ -0,0 +1,32 @@
+package com.codingdojo.codingcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/coding")
+public class CodingControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingControllerApplication.class, args);
+	}
+	
+	@RequestMapping("")
+	public String coding(){
+		return "Hello Coding Dojo!";
+	}
+	
+	@RequestMapping("/python")
+	public String python() {
+		return "Python/Django was awesome!";
+	}
+	
+	@RequestMapping("/java")
+	public String java() {
+		return "Java/Spring is better!";
+	}
+
+}
---
+++ src/main/java/com/codingdojo/codingcontroller/DojoController.java
@@ -0,0 +1,26 @@
+package com.codingdojo.codingcontroller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DojoController {
+
+	@RequestMapping("/{dojo}")
+	public String dojo(@PathVariable("dojo") String dojo) {
+
+		String output = "";
+
+		if (dojo.equals("burbank-dojo")) {
+			output = "Burbank Dojo is located in Southern California";
+		} 
+		else if (dojo.equals("san-jose")) {
+			output = "SJ dojo is the headquarters.";
+		} 
+		else {
+			output = dojo + " is awesome!";
+		}
+		return output;
+	}
+}
